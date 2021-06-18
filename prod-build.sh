86dd98ee169f5d3085857b7c04be638b9893cd9b
Eric Wittmann <eric.wittmann@gmail.com>
2021-06-18 13:59:16 -0400

    debugging build script on github actions

--- prod-build.sh
+++ prod-build.sh
@@ -1,22 +1,43 @@
 #!/bin/sh
 
+echo "yarn install"
+echo "------------"
 yarn install
+
+echo "yarn clean"
+echo "------------"
 yarn clean
+
+echo "yarn build"
+echo "------------"
 yarn build
 
 # Clean the .work directory and then do the apicurio-registry federated module build
 rm -rf .work
 mkdir .work
 cd .work
+
+echo "Checkout apicurio-registry"
+echo "--------------------------"
+pwd
 git clone https://github.com/Apicurio/apicurio-registry.git
 cd apicurio-registry/ui
 yarn install
 yarn run build-mtui
+echo "Success!"
+
+pwd
+ls -al
+ls -al dist
 
 cd ../..
 
 # Copy federated module into dist
+echo "Copying federated module to host 'dist'"
+echo "---------------------------------------"
 mkdir -p dist/modules/registry
+pwd
+find .
 cp -rf .work/apicurio-registry/ui/dist/* dist/modules/registry
 
 
